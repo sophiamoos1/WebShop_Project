e42b611093363dcc1ea835aa8082f0ad59b05092
sophiamoos1 <90751245+sophiamoos1@users.noreply.github.com>
2022-08-18 09:08:35 +0200

    some things

--- webshop/src/Sites/ProductCard.tsx
+++ webshop/src/Sites/ProductCard.tsx
@@ -7,8 +7,17 @@ type Card = {
 
 export default function ProductCard({item}:Card){
     return(
-        <div className="ProductCard">
-
+        <div className="productCard">
+            <img src={item?.picture}/>
+                <div className="cardContent">
+                 <h3 className="productTitle">{item?.title}</h3>
+                    <ol>
+                        <li><span className="description">{item?.description}</span></li>
+                        <li><span className="weight">{item?.weight}</span></li>
+                        <li><span className="price">{item?.weight}</span></li>
+                    </ol>
+                    <button className="button-card"></button>
+            </div>
         </div>
     )
 }
