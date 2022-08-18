8e96853f035de08f79e595193e63b99978143403
sophiamoos1 <90751245+sophiamoos1@users.noreply.github.com>
2022-08-18 11:48:37 +0200

    added register and login button

--- webshop/src/Sites/StartPage.tsx
+++ webshop/src/Sites/StartPage.tsx
@@ -4,8 +4,10 @@ import '../CSS/StartPage.css';
 export default function StartPage(){
     return(
         <div className="startpage">
+            <button className="button-login">Login</button>
             <h2>Random</h2>
             <h2 className="rotate">things</h2>
+            <button className="button-register">Register</button>
         </div>
     );
 }
